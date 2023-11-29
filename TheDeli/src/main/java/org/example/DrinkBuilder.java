f46a517b68f3b1ab7e2209b1efbec4172e2b75ec
Supriya Roberson <supriyaroberson@Supriyas-MBP.home>
2023-11-29 09:29:34 -0600

    some edits

--- TheDeli/src/main/java/org/example/DrinkBuilder.java
+++ TheDeli/src/main/java/org/example/DrinkBuilder.java
@@ -1,5 +1,5 @@
 package org.example;
-
+//Supriya
 public class DrinkBuilder {
     private DrinkSize size;
     private double price = 0.0;
--- TheDeli/src/main/java/org/example/SandwichBuilder.java
+++ TheDeli/src/main/java/org/example/SandwichBuilder.java
@@ -1,50 +1,127 @@
 package org.example;
 
+import java.util.List;
+//Supriya
 public class SandwichBuilder implements Builder{
+    Sandwich sandwich = new Sandwich();
 
-    private Bread breadType;
-    private RegularTopping regularTopping;
-    private Meat meatType;
-    private Cheese cheeseType;
-    private Sauce sauceType;
-    private boolean isToasted;
-    private Side sideType;
-    private BreadLength breadLength;
+    private Bread_Size breadSize;
+    private Bread bread;
+    private Meat meat;
+    private boolean hasExtraMeat;
+    private Cheese cheese;
+    private boolean hasExtraCheese;
+    private List<Regular_Topping> regularTopping;
+    private List<Sauce> sauce;
+    private boolean toasted;
+    private Side side;
+    private double breadSizePrice;
+    private double extraMeatPrice;
+    private double extraCheesePrice;
+    @Override
+    public void setBreadType(Bread bread) {
+        this.bread = bread;
 
-
-    public void setBreadType(Bread breadType) {
-        this.breadType = breadType;
     }
 
+    @Override
+    public void setRegularTopping(List<Regular_Topping> regularTopping) {
+        this.regularTopping= regularTopping;
 
-    public void setRegularTopping(RegularTopping regularTopping) {
-        this.regularTopping = regularTopping;
     }
 
-
-    public void setMeatType(Meat meatType) {
-        this.meatType = meatType;
+    @Override
+    public void setBreadSize(Bread_Size breadSize) {
+        breadSizePrice = 0;
+        this.breadSize = breadSize;
+        switch (breadSize){
+            case FOUR_INCH:
+                breadSizePrice =5.50;
+                break;
+            case EIGHT_INCH:
+                breadSizePrice = 7.00;
+                break;
+            case TWELVE_INCH:
+                breadSizePrice = 8.50;
+                break;
+            default:
+                breadSizePrice= 0;
+        }
     }
 
-
-    public void setCheeseType(Cheese cheeseType) {
-        this.cheeseType = cheeseType;
+    @Override
+    public void setMeatType(Meat meat) {
+        this.meat = meat;
     }
 
-
-    public void setSauceType(Sauce sauceType) {
-        this.sauceType = sauceType;
+    @Override
+    public void setCheeseType(Cheese cheese) {
+        this.cheese = cheese;
     }
 
-
-    public void setToasted(boolean toasted) {
-        isToasted = toasted;
+    @Override
+    public void setSaucesType(List<Sauce> sauce) {
+        this.sauce = sauce;
     }
 
-    public void setSideType(Side sideType) {
-        this.sideType = sideType;
+    @Override
+    public void setSideType(Side side) {
+        this.side = side;
     }
-    public void setBreadLength(BreadLength breadLength) {
-        this.breadLength = breadLength;
+
+    @Override
+    public void isToasted(boolean toasted) {
+        this.toasted = toasted;
+    }
+
+    @Override
+    public void extraMeat(boolean hasExtraMeat, Bread_Size breadSize, int numberOfExtraMeat) {
+        this.hasExtraMeat = hasExtraMeat;
+        extraMeatPrice = 0;
+        this.breadSize = breadSize;
+        if (hasExtraMeat&& numberOfExtraMeat>0){
+            switch (breadSize){
+                case FOUR_INCH:
+                    extraMeatPrice =0.50*numberOfExtraMeat;
+                    break;
+                case EIGHT_INCH:
+                    extraMeatPrice = 1.00*numberOfExtraMeat;
+                    break;
+                case TWELVE_INCH:
+                    extraMeatPrice = 1.50*numberOfExtraMeat;
+                    break;
+                default:
+                    extraMeatPrice= 0;
+                    break;
+            }
+        }
+
+    }
+
+    @Override
+    public void extraCheese(boolean hasExtraCheese, Bread_Size breadSize, int numberOfExtraCheese) {
+        this.hasExtraCheese = hasExtraCheese;
+        extraCheesePrice = 0;
+        this.breadSize = breadSize;
+        if (hasExtraCheese && numberOfExtraCheese >0){
+            switch (breadSize){
+                case FOUR_INCH:
+                    extraCheesePrice =0.30 * numberOfExtraCheese;
+                    break;
+                case EIGHT_INCH:
+                    extraCheesePrice = 0.60 * numberOfExtraCheese;
+                    break;
+                case TWELVE_INCH:
+                    extraCheesePrice = 0.90 * numberOfExtraCheese;
+                    break;
+                default:
+                    extraCheesePrice = 0;
+                    break;
+            }
+        }
+    }
+    public Sandwich buildSandwich(){
+        sandwich.setBasePrice(breadSizePrice+extraMeatPrice+extraCheesePrice);
+        return new Sandwich(breadSize,bread,meat,hasExtraMeat,cheese,hasExtraCheese,regularTopping,sauce,toasted,side,breadSizePrice,extraMeatPrice,extraCheesePrice, sandwich.getBasePrice());
     }
 }
